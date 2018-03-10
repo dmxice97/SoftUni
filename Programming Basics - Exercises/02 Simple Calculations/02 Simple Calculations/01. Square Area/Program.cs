1eadb4d1a2777bd4b97230bb366f03a966de20ef
Kristiyan Topchiyski <dmxice_97@abv.bg>
2018-03-10 15:17:38 +0200

    Exercises

---
+++ Programming Basics - Exercises/02 Simple Calculations/02 Simple Calculations/01. Square Area/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _01._Square_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = double.Parse(Console.ReadLine());
+            var square = a * a;
+            Console.WriteLine("Square = {0}", square);
+
+        }
+    }
+}
---
+++ Programming Basics - Exercises/02 Simple Calculations/02 Simple Calculations/11. USD to BGN/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _11._USD_to_BGN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var usd = double.Parse(Console.ReadLine());
+            var usdTolev = 1.79549;
+            var lev = usd * usdTolev;
+            Console.WriteLine(lev.ToString("f2") + " BGN");
+        }
+    }
+}
