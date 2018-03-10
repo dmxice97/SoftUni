50bd9625278367f1f9800721d9b9e74412d611b7
Kristiyan Topchiyski <dmxice_97@abv.bg>
2018-03-10 13:04:27 +0200

    Exercises

---
+++ Programming Basics - Exercises/01 First Steps in Coding/First Steps in Coding/04. Triangle of 55 Stars/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _04._Triangle_of_55_Stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*");
+            Console.WriteLine("**");
+            Console.WriteLine("***");
+            Console.WriteLine("****");
+            Console.WriteLine("*****");
+            Console.WriteLine("******");
+            Console.WriteLine("*******");
+            Console.WriteLine("********");
+            Console.WriteLine("*********");
+            Console.WriteLine("**********");
+        }
+    }
+}
---
+++ Programming Basics - Exercises/03 Simple Conditional Statements/Simple Cond Statements/01.Excellent Result/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01.Excellent_Result
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
