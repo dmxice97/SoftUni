170ea4b1c3af954f34c852502582b9ecdd138d6d
Kristiyan Topchiyski <dmxice_97@abv.bg>
2018-03-30 11:27:26 +0300

    blog

---
+++ 03 Software Technologies/SoftUniBlog/controllers/home.js
@@ -0,0 +1,13 @@
+module.exports = {
+    index: (req, res) => {
+        Article.findAll({limit: 6, include: [{
+            model: User
+            }]}).then(articles =>{
+            res.render('home/index', {articles: articles});
+        });
+    }
+};
+
+
+
+
---
+++ 03 Software Technologies/SoftUniBlog/models/article.js
@@ -0,0 +1,33 @@
+const Sequelize = require('sequelize');
+
+module.exports = function (sequelize) {
+    const Article = sequelize.define('article', {
+        title: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            required: true
+        },
+        content: {
+            type: Sequelize.TEXT,
+            allowNull: false,
+            required: true
+        },
+        date: {
+            type: Sequelize.DATE,
+            allowNull: false,
+            required: true,
+            dafaultValue: Sequelize.NOW,
+        },
+
+    });
+    Article.associate = function (models) {
+        Article.belongsTo(models.User, {
+            foreignKey: 'autorId',
+            targetKey: 'id'
+        });
+
+    };
+
+    return Article;
+
+};
